2b4bc986d74607aac79b2953c1f774ebe0b5e599
Jakub Gmur <jakubgmur@Op.pl>
2020-03-14 10:43:09 +0100

    1. extended ng start with --open argument to so it opens the browser immediately. 2. added navbar-serrvice that controls nav-menu visibility 3. Home has nav menu visibility set to the false through navbarservice. 4. added bootstrap

--- src/GHTS/GHTS.AngularClient/ClientApp/src/app/home/home.component.ts
+++ src/GHTS/GHTS.AngularClient/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../services/navbar-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  navigationService: NavbarService;
+
+  constructor( public nav: NavbarService ) {
+    this.navigationService = nav;
+  }
+
+
+    ngOnInit(): void {
+        this.navigationService.hide();
+    }
+
 }
--- src/GHTS/GHTS.AngularClient/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/GHTS/GHTS.AngularClient/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NavbarService } from './../services/navbar-service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+  export class NavMenuComponent {
+
+  navigationService: NavbarService;
+
+  constructor( public nav: NavbarService ) {
+    this.navigationService = nav;
+  }
   isExpanded = false;
 
   collapse() {
